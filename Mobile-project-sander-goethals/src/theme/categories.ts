db22d230d13601fab838bc0246eb770fac5dd844
SandrGoethls <sander.goetahls2@student.hogent.be>
2026-01-03 15:44:52 +0100

    feat: update categoryColors to include alpha transparency for improved visual consistency

--- Mobile-project-sander-goethals/src/theme/categories.ts
+++ Mobile-project-sander-goethals/src/theme/categories.ts
@@ -1,23 +1,23 @@
 export const categoryColors = {
   wellness: {
-    first: "rgb(223, 243, 245)",
-    second: "rgb(198, 231, 235)",
-    third: "rgb(127, 202, 211)",
+    first: "rgb(223, 243, 245, 0.8)",
+    second: "rgb(198, 231, 235, 0.8)",
+    third: "rgb(127, 202, 211, 0.8)",
   },
   publicSauna: {
-    first: "rgb(238, 231, 244)",
-    second: "rgb(216, 205, 232)",
-    third: "rgb(155, 134, 184)",
+    first: "rgb(238, 231, 244, 0.8)",
+    second: "rgb(216, 205, 232, 0.8)",
+    third: "rgb(155, 134, 184, 0.8)",
   },
   privateSauna: {
-    first: "rgb(220, 233, 242)",
-    second: "rgb(196, 215, 230)",
-    third: "rgb(127, 169, 201)",
+    first: "rgb(220, 233, 242, 0.8)",
+    second: "rgb(196, 215, 230, 0.8)",
+    third: "rgb(127, 169, 201, 0.8)",
   },
   massageAndBeauty: {
-    first: "rgb(242, 221, 216)",
-    second: "rgb(230, 199, 191)",
-    third: "rgb(178, 124, 118)",
+    first: "rgb(242, 221, 216, 0.8)",
+    second: "rgb(230, 199, 191, 0.8)",
+    third: "rgb(178, 124, 118, 0.8)",
   },
   spaBasic: {
     first: "rgb(190, 225, 255, 0.8)",
@@ -25,18 +25,13 @@ export const categoryColors = {
     third: "rgb(78, 225, 255, 0.8)",
   },
   main: {
-    first: "rgb(200, 218, 211)",
-    second: "rgb(163, 193, 173)",
-    third: "rgb(223, 243, 245)",
+    first: "rgb(200, 218, 211, 0.8)",
+    second: "rgb(163, 193, 173, 0.8)",
+    third: "rgb(223, 243, 245, 0.8)",
   },
   profile: {
-    first: "rgb(226, 214, 191)",
-    second: "rgb(238, 230, 214)",
-    third: "rgb(245, 239, 230)",
-  },
-  login: {
-    buttonColor: "rgb(139, 111, 71)",
-    backgroundColor: "rgb(245, 241, 235)",
-    disabledButtonColor: "rgb(195, 184, 163)",
+    first: "rgb(226, 214, 191, 0.8)",
+    second: "rgb(238, 230, 214, 0.8)",
+    third: "rgb(245, 239, 230, 0.8)",
   },
 } as const;
